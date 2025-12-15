977ddeea3ce2f034ee2e7270765c9939a0e92c61
Borja Fernandez Rodrigo <borja.fernandez.rodrigo@gmail.com>
2025-12-16 00:17:14 +0100

    add: layout

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import './style.css'
+import './css/style.css'
+import { config } from './config/config.js'
+import { I18n } from './js/i18n.js'
+import { Layout } from './js/layout.js'
 
-document.querySelector('#app').innerHTML = `
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-`
+const i18n = new I18n(config.defaultLanguage, config.supportedLanguages)
+await i18n.loadLanguage(config.defaultLanguage)
+
+const layout = new Layout(i18n)
+
+document.getElementById('app').innerHTML = layout.render()
+
+layout.attachEventListeners()
